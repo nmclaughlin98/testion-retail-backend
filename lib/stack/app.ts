1c08b95f95e99ff353b75f212c57be3a3322f7f7
Niall McLaughlin <niallmclaughlin1998@gmail.com>
2026-08-08 19:49:10 +0100

    Fixing regions and roles

--- lib/stack/app.ts
+++ lib/stack/app.ts
@@ -5,6 +5,10 @@ const app = new cdk.App();
 
 new BackendStack(app, 'TestionRetailStack', {
     description: 'TestionRetail Backend Infrastructure',
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT || '334624057595',
+        region: process.env.CDK_DEFAULT_REGION || 'eu-west-2',
+    },
 });
 
 app.synth();
