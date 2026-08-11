8f6166194d58f2da68b66d05026666753e1cc89d
Niall McLaughlin <niallmclaughlin1998@gmail.com>
2026-08-11 18:04:05 +0100

    Updating dynamo, and allowing populating data

--- lib/stack/dynamo-db.ts
+++ lib/stack/dynamo-db.ts
@@ -1,5 +1,5 @@
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export class DatabaseConstruct extends Construct {
@@ -10,9 +10,18 @@ export class DatabaseConstruct extends Construct {
 
         this.table = new dynamodb.Table(this, 'TestionRetailTable', {
             tableName: 'TestionRetail',
-            partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+            partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
+            sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
             billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
             removalPolicy: cdk.RemovalPolicy.DESTROY, // Change to RETAIN for production
+        });
+
+        // GSI 1: Used for querying logins by date/time or looking up users by PIN
+        this.table.addGlobalSecondaryIndex({
+            indexName: 'GSI1',
+            partitionKey: { name: 'GSI1PK', type: dynamodb.AttributeType.STRING },
+            sortKey: { name: 'GSI1SK', type: dynamodb.AttributeType.STRING },
+            projectionType: dynamodb.ProjectionType.ALL,
         });
     }
 }
