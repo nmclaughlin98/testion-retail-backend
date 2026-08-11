a252807e74221f39e0860603661366947cd254f0
Niall McLaughlin <niallmclaughlin1998@gmail.com>
2026-08-11 18:49:57 +0100

    Meh

--- lib/stack/dynamo-db.ts
+++ lib/stack/dynamo-db.ts
@@ -13,10 +13,11 @@ export class DatabaseConstruct extends Construct {
             partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
             sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
             billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-            removalPolicy: cdk.RemovalPolicy.DESTROY, // Change to RETAIN for production
+
+            // Instructs CloudFormation to delete the physical table when destroyed or replaced
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
         });
 
-        // GSI 1: Used for querying logins by date/time or looking up users by PIN
         this.table.addGlobalSecondaryIndex({
             indexName: 'GSI1',
             partitionKey: { name: 'GSI1PK', type: dynamodb.AttributeType.STRING },
